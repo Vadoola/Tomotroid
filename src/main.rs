4876768eccd6e37259d573801e387ceaf95a48ba
Carl Geib <carl@vadoola.com>
2023-06-12 18:01:39 +1200

    Finished adding the logic for the Hyperlink type

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ slint::include_modules!();
 fn main() -> Result<()> {
     let main = Main::new()?;
 
-    let handle1 = main.as_weak();
+    let close_handle = main.as_weak();
     main.on_close_window(move ||{
-        handle1.upgrade().unwrap().hide().unwrap();
+        close_handle.upgrade().unwrap().hide().unwrap();
     });
 
-    let handle2 = main.as_weak();
+    let move_handle = main.as_weak();
     main.on_move_window(move |offset_x, offset_y|{
-        let main = handle2.upgrade().unwrap();
+        let main = move_handle.upgrade().unwrap();
         let logical_pos = main.window().position().to_logical(main.window().scale_factor());
         main.window().set_position(LogicalPosition::new(logical_pos.x + offset_x, logical_pos.y + offset_y));
+    });
+
+    main.global::<HLClick>().on_hl_clicked(|url| {
+        open::that(url.as_str()).unwrap();
     });
 
     main.run()?;
