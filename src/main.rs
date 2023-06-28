b8859137fd55f9c21841445506b5c45aee2a8a85
Carl Geib <carl@vadoola.com>
2023-06-28 21:07:21 +1200

    Added support for the minimize button
    Following the advice on a Slint Discussion, I managed to use the
    internal private Slint API to access the winit window directly. Doing
    this I can call other winit functions on it like minimize. This closes
    Issue #16

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,37 @@ use slint::LogicalPosition;
 slint::include_modules!();
 
 fn main() -> Result<()> {
+    slint::platform::set_platform(Box::new(i_slint_backend_winit::Backend::new())).unwrap();
+
     let main = Main::new()?;
 
     let close_handle = main.as_weak();
-    main.on_close_window(move ||{
+    main.on_close_window(move || {
         close_handle.upgrade().unwrap().hide().unwrap();
+
+        //After I get the system tray working I'm going to want to hide the window instead of actually close it
+        //i_slint_backend_winit::WinitWindowAccessor::with_winit_window(min_handle.window(), |win| win.set_visible(false));
+    });
+
+    let min_handle = main.as_weak();
+    main.on_minimize_window(move || {
+        let min_handle = min_handle.upgrade().unwrap();
+        i_slint_backend_winit::WinitWindowAccessor::with_winit_window(min_handle.window(), |win| {
+            win.set_minimized(true)
+        });
     });
 
     let move_handle = main.as_weak();
-    main.on_move_window(move |offset_x, offset_y|{
-        let main = move_handle.upgrade().unwrap();
-        let logical_pos = main.window().position().to_logical(main.window().scale_factor());
-        main.window().set_position(LogicalPosition::new(logical_pos.x + offset_x, logical_pos.y + offset_y));
+    main.on_move_window(move |offset_x, offset_y| {
+        let move_handle = move_handle.upgrade().unwrap();
+        let logical_pos = move_handle
+            .window()
+            .position()
+            .to_logical(move_handle.window().scale_factor());
+        move_handle.window().set_position(LogicalPosition::new(
+            logical_pos.x + offset_x,
+            logical_pos.y + offset_y,
+        ));
     });
 
     main.global::<HLClick>().on_hl_clicked(|url| {
