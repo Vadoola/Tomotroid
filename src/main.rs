e642c7e0129697210c3a0c79a3bf9dcaf82c5a2a
Carl Geib <carl@vadoola.com>
2023-07-10 22:50:35 +1200

    Early stage work for loading themes from Json
    Just some early (not fully functioning work) to be able to load the
    themes from the Json files that Pomotroid uses, and use those instead
    of the static model I had in the Slint file before.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,80 @@
 use std::io::Cursor;
 
 use anyhow::Result;
-use std::sync::mpsc;
+use std::{
+    rc::Rc,
+    sync::mpsc,
+};
 use tray_item::{IconSource, TrayItem};
+use serde::Deserialize;
+use serde_json::json;
+use hex_color::HexColor;
+use slint::{Color, ModelRc, VecModel};
+
 
 slint::include_modules!();
 
 pub const LOGO_BYTES: &str = include_str!("../assets/logo.svg");
+
+//Right now serde support in Slint is new and crude, some of the types in the Slint version
+//of this struct like Brush don't support serde yet. So for now I'm creating 2 versions
+//the slint version and this version to manually convert between them.
+#[derive(Deserialize)]
+struct ThemeColors {
+    #[serde(rename = "--color-long-round")]
+    long_round: HexColor,
+    
+    #[serde(rename = "--color-short-round")]
+    short_round: HexColor,
+    
+    #[serde(rename = "--color-focus-round")]
+    focus_round: HexColor,
+    
+    #[serde(rename = "--color-background")]
+    background: HexColor,
+    
+    #[serde(rename = "--color-background-light")]
+    background_light: HexColor,
+    
+    #[serde(rename = "--color-background-lightest")]
+    background_lightest: HexColor,
+    
+    #[serde(rename = "--color-foreground")]
+    foreground: HexColor,
+    
+    #[serde(rename = "--color-foreground-darker")]
+    foreground_darker: HexColor,
+    
+    #[serde(rename = "--color-foreground-darkest")]
+    foreground_darkest: HexColor,
+    
+    #[serde(rename = "--color-accent")]
+    accent: HexColor,
+}
+
+#[derive(Deserialize)]
+struct JsonThemeTemp {
+    name: String,
+    colors : ThemeColors,
+}
+
+impl Into<JsonTheme> for JsonThemeTemp {
+    fn into(self) -> JsonTheme {
+        JsonTheme {
+            name: self.name.into(),
+            long_round: Color::from_rgb_u8(self.colors.long_round.r, self.colors.long_round.g, self.colors.long_round.b).into(),
+            short_round: Color::from_rgb_u8(self.colors.short_round.r, self.colors.short_round.g, self.colors.short_round.b).into(),
+            focus_round: Color::from_rgb_u8(self.colors.focus_round.r, self.colors.focus_round.g, self.colors.focus_round.b).into(),
+            background: Color::from_rgb_u8(self.colors.background.r, self.colors.background.g, self.colors.background.b).into(),
+            background_light: Color::from_rgb_u8(self.colors.background_light.r, self.colors.background_light.g, self.colors.background_light.b).into(),
+            background_lightest: Color::from_rgb_u8(self.colors.background_lightest.r, self.colors.background_lightest.g, self.colors.background_lightest.b).into(),
+            foreground: Color::from_rgb_u8(self.colors.foreground.r, self.colors.foreground.g, self.colors.foreground.b).into(),
+            foreground_darker: Color::from_rgb_u8(self.colors.foreground_darker.r, self.colors.foreground_darker.g, self.colors.foreground_darker.b).into(),
+            foreground_darkest: Color::from_rgb_u8(self.colors.foreground_darkest.r, self.colors.foreground_darkest.g, self.colors.foreground_darkest.b).into(),
+            accent: Color::from_rgb_u8(self.colors.accent.r, self.colors.accent.g, self.colors.accent.b).into(),
+        }
+    }
+}
 
 enum TrayMsg {
     MinRes,
@@ -117,11 +185,17 @@ fn main() -> Result<()> {
     });
 
     let thm_handle = main.as_weak();
-    main.global::<ThemeChanged>().on_theme_changed(move |theme| {
+    main.global::<ThemeCallbacks>().on_theme_changed(move |theme| {
         println!("New Theme: {theme}");
         let thm_handle = thm_handle.upgrade().unwrap();
         //just testing that this works...later will need to replace the logo colors with ones from the theme.
         thm_handle.set_logo(slint::Image::load_from_svg_data(LOGO_BYTES.replace("fill:#f6f2eb", "file:#000000").as_bytes()).unwrap());
+    });
+
+    let thmLoad_handle = main.as_weak();
+    main.global::<ThemeCallbacks>().on_load_themes(move || {
+        let model: Rc<VecModel<JsonTheme>> = Rc::new(VecModel::from(vec![]));
+        ModelRc::from(model.clone())
     });
 
     main.run()?;
