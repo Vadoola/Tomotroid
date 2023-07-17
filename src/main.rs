5c54b3f0bb6b4bd411dcc5c91606be2badc20269
Carl Geib <carl@vadoola.com>
2023-07-17 20:06:36 +1200

    Basic loading of themes from Json
    This commit has basic support for loading themes in the Pomotroid Json
    format from a themes directory under the executable folder. It's a bit
    crude at the moment, but it gets the job done.
    
    Need to add support for proper config file locations such as the XDG
    config directory spec etc.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+/*#![warn(
+    clippy::all,
+    clippy::pedantic,
+    //clippy::cargo,
+)]*/
+
 #[cfg(unix)]
 use std::io::Cursor;
 
 use anyhow::Result;
+use hex_color::HexColor;
+use serde::Deserialize;
+use serde_json::json;
+use slint::{Color, ModelRc, VecModel};
 use std::{
+    fs::File,
+    io::{BufReader, Read},
     rc::Rc,
     sync::mpsc,
 };
 use tray_item::{IconSource, TrayItem};
-use serde::Deserialize;
-use serde_json::json;
-use hex_color::HexColor;
-use slint::{Color, ModelRc, VecModel};
-
+use walkdir::WalkDir;
 
 slint::include_modules!();
 
@@ -24,31 +32,31 @@ pub const LOGO_BYTES: &str = include_str!("../assets/logo.svg");
 struct ThemeColors {
     #[serde(rename = "--color-long-round")]
     long_round: HexColor,
-    
+
     #[serde(rename = "--color-short-round")]
     short_round: HexColor,
-    
+
     #[serde(rename = "--color-focus-round")]
     focus_round: HexColor,
-    
+
     #[serde(rename = "--color-background")]
     background: HexColor,
-    
+
     #[serde(rename = "--color-background-light")]
     background_light: HexColor,
-    
+
     #[serde(rename = "--color-background-lightest")]
     background_lightest: HexColor,
-    
+
     #[serde(rename = "--color-foreground")]
     foreground: HexColor,
-    
+
     #[serde(rename = "--color-foreground-darker")]
     foreground_darker: HexColor,
-    
+
     #[serde(rename = "--color-foreground-darkest")]
     foreground_darkest: HexColor,
-    
+
     #[serde(rename = "--color-accent")]
     accent: HexColor,
 }
@@ -56,23 +64,76 @@ struct ThemeColors {
 #[derive(Deserialize)]
 struct JsonThemeTemp {
     name: String,
-    colors : ThemeColors,
+    colors: ThemeColors,
 }
 
+//I realize implemeting From is more idomatic, but that would require creating a newtype for JsonThemeTemp,
+//due to the orphan rule, and then having to convert that (or maybe deref) that into JsonThemeTemp. I think this is a
+//good and straight forward stop gap, until slint adds support for Serde to more types
 impl Into<JsonTheme> for JsonThemeTemp {
     fn into(self) -> JsonTheme {
         JsonTheme {
             name: self.name.into(),
-            long_round: Color::from_rgb_u8(self.colors.long_round.r, self.colors.long_round.g, self.colors.long_round.b).into(),
-            short_round: Color::from_rgb_u8(self.colors.short_round.r, self.colors.short_round.g, self.colors.short_round.b).into(),
-            focus_round: Color::from_rgb_u8(self.colors.focus_round.r, self.colors.focus_round.g, self.colors.focus_round.b).into(),
-            background: Color::from_rgb_u8(self.colors.background.r, self.colors.background.g, self.colors.background.b).into(),
-            background_light: Color::from_rgb_u8(self.colors.background_light.r, self.colors.background_light.g, self.colors.background_light.b).into(),
-            background_lightest: Color::from_rgb_u8(self.colors.background_lightest.r, self.colors.background_lightest.g, self.colors.background_lightest.b).into(),
-            foreground: Color::from_rgb_u8(self.colors.foreground.r, self.colors.foreground.g, self.colors.foreground.b).into(),
-            foreground_darker: Color::from_rgb_u8(self.colors.foreground_darker.r, self.colors.foreground_darker.g, self.colors.foreground_darker.b).into(),
-            foreground_darkest: Color::from_rgb_u8(self.colors.foreground_darkest.r, self.colors.foreground_darkest.g, self.colors.foreground_darkest.b).into(),
-            accent: Color::from_rgb_u8(self.colors.accent.r, self.colors.accent.g, self.colors.accent.b).into(),
+            long_round: Color::from_rgb_u8(
+                self.colors.long_round.r,
+                self.colors.long_round.g,
+                self.colors.long_round.b,
+            )
+            .into(),
+            short_round: Color::from_rgb_u8(
+                self.colors.short_round.r,
+                self.colors.short_round.g,
+                self.colors.short_round.b,
+            )
+            .into(),
+            focus_round: Color::from_rgb_u8(
+                self.colors.focus_round.r,
+                self.colors.focus_round.g,
+                self.colors.focus_round.b,
+            )
+            .into(),
+            background: Color::from_rgb_u8(
+                self.colors.background.r,
+                self.colors.background.g,
+                self.colors.background.b,
+            )
+            .into(),
+            background_light: Color::from_rgb_u8(
+                self.colors.background_light.r,
+                self.colors.background_light.g,
+                self.colors.background_light.b,
+            )
+            .into(),
+            background_lightest: Color::from_rgb_u8(
+                self.colors.background_lightest.r,
+                self.colors.background_lightest.g,
+                self.colors.background_lightest.b,
+            )
+            .into(),
+            foreground: Color::from_rgb_u8(
+                self.colors.foreground.r,
+                self.colors.foreground.g,
+                self.colors.foreground.b,
+            )
+            .into(),
+            foreground_darker: Color::from_rgb_u8(
+                self.colors.foreground_darker.r,
+                self.colors.foreground_darker.g,
+                self.colors.foreground_darker.b,
+            )
+            .into(),
+            foreground_darkest: Color::from_rgb_u8(
+                self.colors.foreground_darkest.r,
+                self.colors.foreground_darkest.g,
+                self.colors.foreground_darkest.b,
+            )
+            .into(),
+            accent: Color::from_rgb_u8(
+                self.colors.accent.r,
+                self.colors.accent.g,
+                self.colors.accent.b,
+            )
+            .into(),
         }
     }
 }
@@ -101,46 +162,44 @@ fn main() -> Result<()> {
             height: 32,
             width: 32,
         };
-        TrayItem::new(
-            "Tomotroid\nClick to Restore",
-            logo_icon
-        ).unwrap()
+        TrayItem::new("Tomotroid\nClick to Restore", logo_icon).unwrap()
     };
 
     #[cfg(windows)]
     let mut tray = TrayItem::new(
         "Tomotroid\nClick to Restore",
         IconSource::Resource("logo-icon"),
-    ).unwrap();
+    )
+    .unwrap();
 
     let (tray_tx, tray_rx) = mpsc::sync_channel(1);
 
     let minres_tx = tray_tx.clone();
     tray.add_menu_item("Minimize / Restore", move || {
         minres_tx.send(TrayMsg::MinRes).unwrap();
-    }).unwrap();
+    })
+    .unwrap();
 
-    let quit_tx = tray_tx;//.clone();
+    let quit_tx = tray_tx; //.clone();
     tray.add_menu_item("Quit", move || {
         quit_tx.send(TrayMsg::Quit).unwrap();
-    }).unwrap();
+    })
+    .unwrap();
 
     slint::platform::set_platform(Box::new(i_slint_backend_winit::Backend::new())).unwrap();
 
     let main = Main::new()?;
 
     let close_handle = main.as_weak();
-    main.on_close_window(move ||{
+    main.on_close_window(move || {
         close_handle.upgrade().unwrap().hide().unwrap();
 
         //After I get the system tray working I'm going to want to hide the window instead of actually close it
         //i_slint_backend_winit::WinitWindowAccessor::with_winit_window(min_handle.window(), |win| win.set_visible(false));
     });
 
-    
-
     let min_handle = main.as_weak();
-    main.on_minimize_window(move ||{
+    main.on_minimize_window(move || {
         let min_handle = min_handle.upgrade().unwrap();
         i_slint_backend_winit::WinitWindowAccessor::with_winit_window(min_handle.window(), |win| {
             win.set_minimized(true);
@@ -148,35 +207,39 @@ fn main() -> Result<()> {
     });
 
     let move_handle = main.as_weak();
-    main.on_move_window(move ||{
+    main.on_move_window(move || {
         let move_handle = move_handle.upgrade().unwrap();
-        i_slint_backend_winit::WinitWindowAccessor::with_winit_window(move_handle.window(), |win| win.drag_window());
+        i_slint_backend_winit::WinitWindowAccessor::with_winit_window(
+            move_handle.window(),
+            |win| win.drag_window(),
+        );
     });
 
     let tray_handle = main.as_weak();
-    let _tray_rec_thread = std::thread::spawn(move || {
-        loop {
-            match tray_rx.recv() {
-                Ok(TrayMsg::MinRes) => {
-                    let tray_handle_copy = tray_handle.clone();
-                    slint::invoke_from_event_loop(move || {
-                        let main = tray_handle_copy.upgrade().unwrap();
-                        i_slint_backend_winit::WinitWindowAccessor::with_winit_window(main.window(),
-                            |win| {
-                                win.set_minimized(!win.is_minimized().unwrap());
-                                win.focus_window();
-                            }
-                        );
-                    }).unwrap();
-                }
-                Ok(TrayMsg::Quit) => {
-                    let tray_handle_copy = tray_handle.clone();
-                    slint::invoke_from_event_loop(move || {
-                        tray_handle_copy.upgrade().unwrap().hide().unwrap();
-                    }).unwrap();
-                }
-                _ => {}
+    let _tray_rec_thread = std::thread::spawn(move || loop {
+        match tray_rx.recv() {
+            Ok(TrayMsg::MinRes) => {
+                let tray_handle_copy = tray_handle.clone();
+                slint::invoke_from_event_loop(move || {
+                    let main = tray_handle_copy.upgrade().unwrap();
+                    i_slint_backend_winit::WinitWindowAccessor::with_winit_window(
+                        main.window(),
+                        |win| {
+                            win.set_minimized(!win.is_minimized().unwrap());
+                            win.focus_window();
+                        },
+                    );
+                })
+                .unwrap();
             }
+            Ok(TrayMsg::Quit) => {
+                let tray_handle_copy = tray_handle.clone();
+                slint::invoke_from_event_loop(move || {
+                    tray_handle_copy.upgrade().unwrap().hide().unwrap();
+                })
+                .unwrap();
+            }
+            _ => {}
         }
     });
 
@@ -185,19 +248,57 @@ fn main() -> Result<()> {
     });
 
     let thm_handle = main.as_weak();
-    main.global::<ThemeCallbacks>().on_theme_changed(move |theme| {
-        println!("New Theme: {theme}");
-        let thm_handle = thm_handle.upgrade().unwrap();
-        //just testing that this works...later will need to replace the logo colors with ones from the theme.
-        thm_handle.set_logo(slint::Image::load_from_svg_data(LOGO_BYTES.replace("fill:#f6f2eb", "file:#000000").as_bytes()).unwrap());
-    });
+    main.global::<ThemeCallbacks>()
+        .on_theme_changed(move |theme| {
+            let thm_handle = thm_handle.upgrade().unwrap();
+            //just testing that this works...later will need to replace the logo colors with ones from the theme.
+            thm_handle.set_logo(
+                slint::Image::load_from_svg_data(
+                    LOGO_BYTES
+                        .replace("fill:#f6f2eb", "file:#000000")
+                        .as_bytes(),
+                )
+                .unwrap(),
+            );
+        });
 
-    let thmLoad_handle = main.as_weak();
     main.global::<ThemeCallbacks>().on_load_themes(move || {
-        let model: Rc<VecModel<JsonTheme>> = Rc::new(VecModel::from(vec![]));
+        let mut theme_dir = std::env::current_dir().unwrap();
+        theme_dir.push("themes");
+        let themes: Vec<JsonTheme> = {
+            let mut themes: Vec<JsonTheme> = WalkDir::new(theme_dir)
+                .into_iter()
+                .filter(|e| {
+                    println!("{:#?}", e.as_ref().unwrap().path());
+                    return e.as_ref().map_or(false, |f| {
+                        f.file_name()
+                            .to_str()
+                            .map(|s| s.ends_with(".json"))
+                            .unwrap_or(false)
+                    });
+                })
+                .filter_map(|e| {
+                    e.map(|e| {
+                        let reader = BufReader::new(File::open(e.path()).unwrap());
+                        let theme = std::io::read_to_string(reader).unwrap();
+                        let theme = serde_json::from_str::<JsonThemeTemp>(&theme)
+                            .unwrap()
+                            .into();
+                        theme
+                    })
+                    .ok()
+                })
+                .collect();
+            themes.sort_by(|a, b| a.name.partial_cmp(&b.name).unwrap());
+            themes
+        };
+
+        let model: Rc<VecModel<JsonTheme>> = Rc::new(VecModel::from(themes));
+
         ModelRc::from(model.clone())
     });
 
+    main.global::<ThemeCallbacks>().invoke_load_themes();
     main.run()?;
     Ok(())
 }
